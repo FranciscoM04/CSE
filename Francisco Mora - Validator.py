760c55e93b204f81f9bd85a545c7274c12436ff6
FranciscoM04 <42385509+FranciscoM04@users.noreply.github.com>
2019-05-14 09:42:38 -0700

    5/14/19

--- Francisco Mora - Validator.py
+++ Francisco Mora - Validator.py
@@ -1,27 +1,23 @@
 import csv
 
 def valid_card_number(num: str):
-
-print(valid_card_number("9988951394582520"))
-
-with open("Book1.csv") as old_csv:
-    with open("MyNewFile.csv", 'w', newline='') as new_csv:
-        reader = csv.reader(old_csv)
-    writer = csv.writer(new_csv)
-    print("Processing...")
-
-    for row in reader:
-        # old_number = int(row[0]) + 1
-        old_number = row[0]
-        if validate(old_number):
-            writer.writerow(row)
-print("Done")
-
-def valid_card_number(num: str):
-    remove_last_digit(num)
+    if len(num) != 16:
+        return False
+    last_digit = int(num[15])
+    other_nums = num[0:15]
+    print(other_nums)
 
 
-def remove_last_digit():
+print(valid_card_number("4556737586899855"))
 
-
-print(valid_card_number("9988951394582520"))
+# with open("Book1.csv") as old_csv:
+#         with open("MyNewFile.csv", 'w', newline='') as new_csv:
+#             reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#         print("Processing...")
+#
+#         for row in reader:
+#             # old_number = int(row[0]) + 1
+#             old_number = row[0]
+#             if valid_card_number(old_number):
+#                 writer.writerow(row)
